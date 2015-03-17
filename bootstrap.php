e3eddc7b8c7b30752058a244761d309bcad3489d
Milan Felix Sulc <rkfelix@gmail.com>
2015-03-17 03:45:19 +0100

    Travis CI: update config

--- bootstrap.php
+++ bootstrap.php
@@ -29,8 +29,8 @@ final class Bootstrap
 
     public static function result($library, $libraryVersion, $startTime, $endTime)
     {
-        echo str_pad($library, 12) . ' | ' .
-            str_pad($libraryVersion, 15) . ' | ' .
+        echo str_pad($library, 13) . ' | ' .
+            str_pad($libraryVersion, 20) . ' | ' .
             'Time: ', str_pad(sprintf('%.3f', $startTime + $endTime), 6, ' ', STR_PAD_LEFT), ' s | ',
         'Memory: ', str_pad(sprintf('%.1f', memory_get_peak_usage() / 1024 / 1024), 4, ' ', STR_PAD_LEFT), ' MB | ',
         'PHP: ', PHP_VERSION . PHP_EOL;
