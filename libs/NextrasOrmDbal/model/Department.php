cc8595c8a0563b574b2ab0637adfa36fef9e02a4
Jan Škrášek <hrach.cz@gmail.com>
2015-03-20 14:10:39 +0100

    nextras orm dbal: fixed type of primary key

--- libs/NextrasOrmDbal/model/Department.php
+++ libs/NextrasOrmDbal/model/Department.php
@@ -6,6 +6,7 @@ use Nextras\Orm\Entity\Entity;
 use Nextras\Orm\Relationships\ManyHasMany;
 
 /**
+ * @property string|NULL $id
  * @property string $name
  * @property ManyHasMany|Employee[] $employees {m:n EmployeesRepository $departments}
  */
